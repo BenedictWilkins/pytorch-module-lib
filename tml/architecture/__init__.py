8e51372627cf5543fc88e0d70d406f8552c2c14a
Benedict Wilkins <benrjw@gmail.com>
2022-04-08 13:30:00 +0100

    more modules

---
+++ tml/architecture/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" 
+   Created on 08-04-2022
+"""
+__author__ = "Benedict Wilkins"
+__email__ = "benrjw@gmail.com"
+__status__ = "Development"
+
+from . import AlexNet
